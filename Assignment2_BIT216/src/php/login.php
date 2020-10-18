8b08035d1798f0c4c477e42410a0d9fd7eac09d6
Ho Ken Wye Ryan <ryankwho@gmail.com>
2020-10-19 02:23:57 +0800

    Revert back to working state.

--- Assignment2_BIT216/src/php/login.php
+++ Assignment2_BIT216/src/php/login.php
@@ -4,11 +4,18 @@ $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 if(isset($postdata) && !empty($postdata))
 {
+<<<<<<< HEAD
 	$password = mysqli_real_escape_string($mysqli, trim($request->password));
   $username = mysqli_real_escape_string($mysqli, trim($request->username));
 $sql='';
 
 	$sql = "SELECT * FROM user where username='$username' and password ='$password'";
+=======
+	$pwd = mysqli_real_escape_string($mysqli, trim($request->password));
+   $email = mysqli_real_escape_string($mysqli, trim($request->username));
+$sql='';
+	$sql = "SELECT * FROM employee where email='$email' and pwd='$pwd'";
+>>>>>>> parent of 0076b1c... login.php and registration.php changes as per A2 requirements.
 if($result = mysqli_query($mysqli,$sql))
 {
  $rows = array();
@@ -16,7 +23,7 @@ if($result = mysqli_query($mysqli,$sql))
   {
     $rows[] = $row;
   }
-
+ 
   echo json_encode($rows);
 }
 else
